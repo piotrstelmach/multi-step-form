94eeab3e03128daa96a8c792795f41a0d4be341a
Paweł Leśnik <lesnik.pawel1991@gmail.com>
2023-04-15 11:20:55 +0200

    change input value

--- src/components/FirstStep/FirstStep.tsx
+++ src/components/FirstStep/FirstStep.tsx
@@ -1,23 +1,26 @@
-export const FirstStep = ({register}:any) => {
-
-  
-  
-  
-    return (
-      <div >
-        step 1
-        <div className="input-wrapper">
-       <label htmlFor="name">
+export const FirstStep = ({ register }: any) => {
+  return (
+    <div>
+      step 1
+      <div className="input-wrapper">
+        <label htmlFor="firstName">
           Name
-         <input id="name" defaultValue="" {...register("name",{required:true})} />
-         </label>
-  
-         <label htmlFor="lastName">
+          <input
+            id="firstName"
+            defaultValue=""
+            {...register("firstName", { required: true })}
+          />
+        </label>
+
+        <label htmlFor="lastName">
           Last Name
-         <input id="lastName" defaultValue="" {...register("lastName",{required:true})} />
-         </label>
-       </div>
-  
+          <input
+            id="lastName"
+            defaultValue=""
+            {...register("lastName", { required: true })}
+          />
+        </label>
+      </div>
     </div>
-    )
-  }
+  );
+};
